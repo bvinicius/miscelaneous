0a410f76e5a4a2b30d5fdb0319a9bfad7df50c98
vbazanella <vinicius.bazanella1@gmail.com>
2020-01-05 16:09:28 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+import { MatSidenav } from '@angular/material'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'weather-angular';
+  @ViewChild(MatSidenav, {static: false}) sidenav: MatSidenav;
+
+  title: 'Miscelaneous'  
+
+  constructor(private router: Router) {
+  }
+
+  toggleSidenav() {
+    this.sidenav.toggle()
+  }
+
+  goTo(s:string) {
+    this.router.navigate([s])
+
+    this.sidenav.toggle()
+    console.log(this.router)
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout'
+
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
