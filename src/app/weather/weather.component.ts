032c5c0604a2792dc87f9e531af6fa7917252217
vbazanella <vinicius.bazanella1@gmail.com>
2020-01-07 20:50:25 -0300

    Implemented city search term

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, ViewChildren } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable, Subscription } from 'rxjs';
+import { MatInput } from '@angular/material';
 
 @Component({
   selector: 'app-weather',
@@ -11,12 +12,44 @@ export class WeatherComponent implements OnInit {
   subscriptions: Subscription[] = []
   countries: String[] = []
 
+  city: any
+
+  @ViewChild(MatInput, {static: false}) input: MatInput;
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {this.input.focus()})
   }
 
-  onChange(value:string) {
+  fetchWeather(city:string) {
+    this.input.value = ''
+
+    const term = city
+      .split(' ')
+      .join('+')
+
+    console.log(term)
+    
+    this.http.get(`https://community-open-weather-map.p.rapidapi.com/weather?q=${term}&units=metric`, 
+      {
+        headers: {	
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          "x-rapidapi-key": "dac1bfeac6msh81fea4f48af0f77p162317jsn2d9f313ea36f"
+        }
+      })
+      .subscribe((res:any) => {
+        console.log(res)
+        this.city = {
+          name: res.name,
+          country: res.sys.country,
+          temperature: res.main.temp
+        }
+      })
+  }
+
+    onChange(value:string) {
     const term = value.toLowerCase()
     this.subscriptions.push(
       this.http.get('')
@@ -25,14 +58,6 @@ export class WeatherComponent implements OnInit {
           console.log(res)
         }, error => {})
     )  
-  }
-
-  fetchWeather() {
-    console.log('fetched')
-  }
-
-  fetchCountries() {
-    
   }
 
   ngOnDestroy() {
